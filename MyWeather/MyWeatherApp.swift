dc2ad834097648a84021bcb834164529cb08449a
Gia Nguyen <nhgia.apcs@gmail.com>
2021-11-17 16:16:04 +0700

    Init commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Gia Nguyen on 13/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
